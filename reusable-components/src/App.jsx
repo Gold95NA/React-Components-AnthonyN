dd89d79ba356e90fb9522ee004d60c678ee8bbbc
Gold95NA <156334092+Gold95NA@users.noreply.github.com>
2025-05-01 05:55:45 -0400

    commit

---
+++ reusable-components/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MyAwesomeComponent from "./components/MyAwesomeComponent";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <MyAwesomeComponent />
+    </div>
+  );
+}
+
+export default App;
